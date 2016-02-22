5327570e664cefdec16d5b3ae008df047175c409
tkr237 <trentkrogers@gmail.com>
2016-02-22 14:09:33 -0600

    Initial commit

---
+++ ASP.NetBoxService/ASP.NetBoxService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NetBoxService.Startup))]
+namespace ASP.NetBoxService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
