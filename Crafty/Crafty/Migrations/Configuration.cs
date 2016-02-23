c855f724c0ee86ae194368527678a2b69b35736b
tkr237 <trentkrogers@gmail.com>
2016-02-23 15:06:40 -0600

    working on controllers and database

---
+++ Crafty/Crafty/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Crafty.Migrations
+{
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Crafty.Models.RegisteredUserDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Crafty.Models.RegisteredUserDBContext";
+        }
+
+        protected override void Seed(Crafty.Models.RegisteredUserDBContext context)
+        {
+           // This method will be called after migrating to the latest version.
+
+           // You can use the DbSet<T>.AddOrUpdate() helper extension method
+           // to avoid creating duplicate seed data.E.g.
+
+              context.RegisteredUsers.AddOrUpdate(
+                p => p.name,
+                new RegisteredUser { name = "Eazy=E", address = "Compton", subscriptionCost = 500, totalSubscriptionCost = 1000},
+                new RegisteredUser { name = "test", address = "333 Plankinton", subscriptionCost = 800, totalSubscriptionCost = 2000 }
+              );
+
+        }
+    }
+}
---
+++ Crafty/Crafty/Models/SurveyModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Crafty.Models
+{
+    public class RegisteredUser
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public int subscriptionCost { get; set; }
+        public int totalSubscriptionCost { get; set; }
+        //add "demographic"
+    }
+
+    public class RegisteredUserDBContext : DbContext
+    {
+        public DbSet<RegisteredUser> RegisteredUsers { get; set; }
+        //this is where we can add more databases
+    }
+
+
+
+
+
+    //public class SurveyModel
+    //{
+    //    public int ID { get; set; }
+    //    public int q1 { get; set; }
+    //    public int q2 { get; set; }
+    //}
+
+    //public class Survey : DbContext
+    //{
+    //    public Survey() : base("SurveyResponses")
+    //    {
+
+    //    }
+
+    //    public DbSet<SurveyModel> Surveys { get; set; }
+
+    //    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    //    {
+    //        modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+    //    }
+    //}
+
+    ////not sure if below code is ever used
+    //public class SurveyInitializer : System.Data.Entity. DropCreateDatabaseIfModelChanges<Survey>
+    //{
+    //    protected override void Seed(Survey context)
+    //    {
+    //        var surveyResponses = new List<SurveyModel>
+    //        {
+    //            new SurveyModel {ID = 1, q1=10, q2=5 },
+    //            new SurveyModel {ID = 2, q1=5, q2=6 },
+    //        };
+
+    //        surveyResponses.ForEach(s => context.Surveys.Add(s));
+    //        context.SaveChanges();
+    //    }
+    //}
+}
