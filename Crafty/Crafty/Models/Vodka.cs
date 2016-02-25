599887af361028e371d053b73d5e6eb29ccfc6e5
tkr237 <trentkrogers@gmail.com>
2016-02-25 15:46:20 -0600

    Added product classes

---
+++ Crafty/Crafty/Models/Vodka.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crafty.Models
+{
+    public class Vodka : AlcoholProduct
+    {
+        public string name = "Vodka";
+    }
+}
